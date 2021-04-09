52cf380f3c2294facd942ff54c3f6971f3761b7e
SimonFJ20 <simonfromjakobsen@gmail.com>
2021-04-09 09:58:02 +0200

    scratch

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,7 @@
-import { calculator } from './calculator';
-import { calculator2 } from './calculator2';
-import { graphing } from './graphing';
 
 const main = () => {
     
-    const page = window.location.pathname.split('/').pop();
     
-    switch(page) {
-        
-        case 'calculator.html': {
-            calculator();
-            break;
-        }
-
-        case 'calculator2.html': {
-            calculator2();
-            break;
-        }
-        
-        case 'graphing.html': {
-            graphing();
-            break;
-        }
-
-        // experimental version
-        case 'shiba.html': {
-            // shiba
-            break;
-        }
-        
-        default: {
-            throw new Error('File not found!');
-        }
-        
-    }
     
 }
 
